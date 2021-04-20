6a4a5f6fcca427bf4467de910800bbf5362a1e46
Pk-mudaliar <--pkmudaliar9669@gmail.com>
2021-04-20 11:04:51 +0530

    frontend completed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Blocks from './Blocks';
+import {Link} from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 
@@ -7,7 +7,7 @@ class App extends Component{
     state = {walletInfo : {}};
 
     componentDidMount() {
-        fetch('http://localhost:3000/api/wallet-info')
+        fetch(`${document.location.origin}/api/wallet-info`)
         .then(response => response.json())
         .then(json => this.setState({walletInfo: json}));
     }
@@ -23,12 +23,14 @@ class App extends Component{
                 Welcome to the blockchain !
                 </div>
                 <br />
+                <div><Link to='/blocks'>Blocks</Link></div>
+                <div><Link to='/conduct-transaction'>Conduct a Transaction</Link></div>
+                <div><Link to='/transaction-pool'>Transaction Pool</Link></div>
+                <br />
                 <div className='WalletInfo'>
                     <div>Address: {address}</div>
                     <div>Balance: {balance}</div>
                 </div>
-                <br />
-                <Blocks />
             </div>
         );
     }
